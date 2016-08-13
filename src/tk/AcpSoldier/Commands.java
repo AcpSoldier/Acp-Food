55440793a3d4fd6cf9d17807d393bc6496e3541a
Thomas Knapp <thethomasknapp@gmail.com>
2016-08-13 19:24:50 -0400

    Fixed consoles not being able to send commands.
    Now the user can do /acpfood reload from the console. Also removed some
    unused instances I forgot about :smile:

--- src/tk/AcpSoldier/Commands.java
+++ src/tk/AcpSoldier/Commands.java
@@ -6,10 +6,6 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import tk.AcpSoldier.food.foods.CannedBeans;
-import tk.AcpSoldier.food.foods.CannedPasta;
-import tk.AcpSoldier.food.foods.Pepsi;
-
 public class Commands implements CommandExecutor {
 
 	AcpFood acpFood;
@@ -18,34 +14,49 @@ public class Commands implements CommandExecutor {
 		this.acpFood = acpFood;
 	}
 
-	CannedBeans cannedBeans = new CannedBeans(acpFood);
-	CannedPasta cannedPasta = new CannedPasta(acpFood);
-	Pepsi pepsi = new Pepsi(acpFood);
-
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String Label, String[] args) {
-		
-		String cmd = command.getName();
-		Player p = (Player) sender;
-		
-		if (cmd.equalsIgnoreCase("acpfood")) {
-			if (args.length == 1) {
 
+		String c = command.getName();
+
+		if (c.equalsIgnoreCase("acpfood")) {
+			if (args.length == 1) {
 				if (args[0].equalsIgnoreCase("reload")) {
-					
-					p.sendMessage(ChatColor.GOLD + "Reloading Acp Food settings...");
-					acpFood.reload();
-					p.sendMessage(ChatColor.GOLD + "Acp Food settings have been updated!");
+
+					if (sender instanceof Player) {
+
+						Player p = (Player) sender;
+						p.sendMessage(ChatColor.GOLD + "Reloading Acp Food settings...");
+						acpFood.reload();
+						p.sendMessage(ChatColor.GOLD + "Acp Food settings have been updated!");
+					}
+					else {
+
+						acpFood.getLogger().info("Reloading Acp Food settings...");
+						acpFood.reload();
+						acpFood.getLogger().info("Acp Food settings have been updated!");
+					}
 				}
 				else {
-					
-					p.sendMessage(ChatColor.GOLD + "Uhhh, you wut m8? (Unknown command)");
+					if (sender instanceof Player) {
+
+						Player p = (Player) sender;
+						p.sendMessage(ChatColor.GOLD + "Uhhhh, you wut m8? (Unknown command)");
+					}
+					else {
+						acpFood.getLogger().info("Uhhhh, you wut m8? (Unknown command)");
+					}
 				}
 			}
 			else {
-				
-				p.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "AcpFood v" + acpFood.version + " made by AcpSoldier.");
-				p.sendMessage(ChatColor.GOLD + "Need custom plugins? www.AcpSoldier.TK");
+				if (sender instanceof Player) {
+
+					Player p = (Player) sender;
+					p.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "AcpFood v" + acpFood.version + " made by AcpSoldier.");
+				}
+					else {
+					acpFood.getLogger().info("AcpFood v" + acpFood.version + " made by AcpSoldier.");
+				}
 			}
 		}
 		return true;
